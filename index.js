c4f2ab08972b6466093175c53c49a62cd8f5a136
Carlos Davi Dantas <94587505+carlosdavidantas@users.noreply.github.com>
2024-08-28 21:37:31 -0300

    init: arquivos do projeto adicionado

---
+++ index.js
@@ -0,0 +1,8 @@
+
+document.getElementById("contact-button").addEventListener("click", () => {
+    document.querySelector(".form-background").scrollIntoView();
+});
+
+document.getElementById("home-button").addEventListener("click", () => {
+    document.querySelector(".background").scrollIntoView();
+});
